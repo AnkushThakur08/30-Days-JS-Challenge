f121a1b862a1f6968c9b393b5484a58fd8184864
Ankush Thakur <atpara99@gmail.com>
2024-07-14 13:48:26 +0530

    Day 1 Completed

---
+++ Day_1/13th_July.js
@@ -0,0 +1,43 @@
+// TODO: Activity 1
+/* var number = 10;
+console.log(number); 
+
+ let name = "Ankush";
+console.log(name); */
+
+// TODO: Activity 2
+/* const isLoggedIn = false;
+console.log(isLoggedIn); */
+
+//  TODO: Activity 3
+/* const age = 25;
+const fullName = "Ankush Thakur";
+const isGoogleSignedIn = false;
+const userInfo = {
+  name: fullName,
+  age: age,
+  isGoogleSignedIn: isGoogleSignedIn,
+};
+const courses = ["React", "Node", "MongoDB", "Express"];
+
+console.log(typeof age);
+console.log(typeof fullName);
+console.log(typeof isGoogleSignedIn);
+console.log(typeof userInfo);
+console.log(typeof courses); */
+
+//  TODO: Activity 4
+/* let number = 50;
+console.log(number);
+
+number = 80;
+
+console.log(number); */
+
+// TODO: Activity 5
+/* const fullName = "Ankush Thakur";
+
+fullName = "Hitesh Sir";
+
+console.log(fullName);
+ */
