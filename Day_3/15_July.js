3a1eb5cdcccfdb82431a5fe3074b704e898670a6
Ankush Thakur <atpara99@gmail.com>
2024-07-15 21:59:32 +0530

    Day 3 Completed

---
+++ Day_3/15_July.js
@@ -0,0 +1,119 @@
+// TODO: Activity 1
+/* const numberChecker = (num) => {
+  if (num > 0) {
+    return "Number is +ve";
+  } else if (num < 0) {
+    return "Number is -ve";
+  } else {
+    return "Number is 0";
+  }
+};
+
+console.log(numberChecker(10));
+console.log(numberChecker(-10));
+console.log(numberChecker(0)); */
+
+/* const checkVoteEligibility = (value) => {
+  if (value >= 18) {
+    return "You are Allowed to vote";
+  } else {
+    return "You are not Allowed to vote";
+  }
+};
+
+console.log(checkVoteEligibility(16));
+console.log(checkVoteEligibility(20));
+ */
+
+// TODO: Activity 2
+/* const findLargestNumber = (num1, num2, num3) => {
+  if (num1 > num2) {
+    if (num1 > num3) {
+      return "Number 1 is the greatest";
+    } else {
+      return "Number 3 is the greatest";
+    }
+  } else {
+    if (num2 > num3) {
+      return "Number 2 is the greatest";
+    } else {
+      return "Number 3 is the greatest";
+    }
+  }
+};
+
+console.log(findLargestNumber(50, 20, 30));
+console.log(findLargestNumber(20, 50, 30));
+console.log(findLargestNumber(20, 30, 50));
+ */
+
+// TODO: Activity 3
+/* const dayOfWeek = (day) => {
+  switch (day) {
+    case 1:
+      return "Monday";
+    case 2:
+      return "Tuesday";
+    case 3:
+      return "Wednesday";
+    case 4:
+      return "Thursday";
+    case 5:
+      return "Friday";
+    case 6:
+      return "Saturday";
+    case 7:
+      return "Sunday";
+    default:
+      return "Invalid Input";
+  }
+};
+
+console.log(dayOfWeek(1));
+console.log(dayOfWeek(2));
+console.log(dayOfWeek(3));
+console.log(dayOfWeek(4));
+console.log(dayOfWeek(5));
+console.log(dayOfWeek(6));
+console.log(dayOfWeek(7)); */
+/* const gradeCalculter = (marks) => {
+  switch (true) {
+    case marks >= 90:
+      return "A";
+    case marks >= 80:
+      return "B";
+    case marks >= 60:
+      return "C";
+    case marks >= 40:
+      return "D";
+    default:
+      return "F";
+  }
+};
+
+console.log(gradeCalculter(93));
+console.log(gradeCalculter(60));
+console.log(gradeCalculter(40));
+console.log(gradeCalculter(100));
+console.log(gradeCalculter(30)); */
+
+// TODO: Activity 4
+/* const isEven = (num) => {
+  return num % 2 === 0 ? "Number is Even" : "Number is Odd";
+};
+
+console.log(isEven(25));
+console.log(isEven(20)); */
+
+// TODO: Activity 5
+/* const leapYearChecker = (year) => {
+  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+    return "LEAP YEAR";
+  } else {
+    return "NOT a leap year";
+  }
+};
+
+console.log(leapYearChecker(1998));
+console.log(leapYearChecker(2028));
+ */
