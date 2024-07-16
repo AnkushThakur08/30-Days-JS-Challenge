7c650309cbf1708e4b52f8ec2d55d86d8b1ae73b
Ankush Thakur <atpara99@gmail.com>
2024-07-16 22:54:26 +0530

    Day 4

---
+++ Day_4/16_July.js
@@ -0,0 +1,54 @@
+// TODO: Activity 1 : Print numbers from 1 to 10 using for loop, while loop
+/* for (let i = 1; i <= 10; i++) {
+  console.log(i);
+} */
+
+/* let i = 1;
+while (i <= 10) {
+  console.log(i);
+  i++;
+} */
+
+// Mutiplication Table
+/* for (let i = 1; i <= 10; i++) {
+  console.log(`${5} * ${i} = ${5 * i} `);
+} */
+
+// TODO: Activity 2 : Calculate the sum for 1-10 using while loop
+/* let sum = 0;
+let i = 0;
+while (i <= 10) {
+  sum = sum + i;
+  i++;
+}
+console.log("Sum is " + sum); */
+
+// Print numbers from 10 to 1 using for loop, while loop
+/* let n = 10;
+while (n >= 1) {
+  console.log(n);
+  n--;
+} */
+
+// TODO: Activity 3
+/* let i = 1;
+do {
+  console.log(i);
+  i++;
+} while (i <= 5); */
+
+/* function factorial(n) {
+  let result = 1;
+  let i = n;
+
+  do {
+    result *= i;
+    i--;
+  } while (i > 0);
+
+  return result;
+}
+
+const number = 5;
+const fact = factorial(number);
+console.log(`The factorial of ${number} is ${fact}`); */
