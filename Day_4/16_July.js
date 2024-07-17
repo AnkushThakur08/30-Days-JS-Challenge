f7c66c30b695360dad46f73d450fba0d641d2421
Ankush Thakur <atpara99@gmail.com>
2024-07-17 08:48:58 +0530

    Day 4 Completed

--- Day_4/16_July.js
+++ Day_4/16_July.js
@@ -3,6 +3,7 @@
   console.log(i);
 } */
 
+// Print numbers from 1 to 10 using while loop
 /* let i = 1;
 while (i <= 10) {
   console.log(i);
@@ -31,24 +32,60 @@ while (n >= 1) {
 } */
 
 // TODO: Activity 3
+// Print Number from 1 to 5 using a do...while loop
 /* let i = 1;
 do {
   console.log(i);
   i++;
 } while (i <= 5); */
 
-/* function factorial(n) {
+// Program to calculate Factorial of a number using a do...while loop
+/* const factorial = (n) => {
   let result = 1;
   let i = n;
 
   do {
-    result *= i;
+    result = result * i;
     i--;
   } while (i > 0);
 
   return result;
+};
+
+console.log(factorial(5));
+ */
+
+// TODO: Activity 4 Print a pattern using nested loop
+
+/* for (i = 1; i <= 5; i++) {
+  let star = "*";
+  console.log(star.repeat(i));
+} */
+
+/* function printLeftAngledTriangle(rows) {
+  for (let i = 1; i <= rows; i++) {
+    let output = "";
+    for (let j = 1; j <= i; j++) {
+      output = output + "*";
+    }
+    console.log(output);
+  }
 }
 
-const number = 5;
-const fact = factorial(number);
-console.log(`The factorial of ${number} is ${fact}`); */
+printLeftAngledTriangle(5); */
+
+// TODO: Activity 5 Print 1 to 10 , skip 5 using continue
+/* for (i = 1; i <= 10; i++) {
+  if (i === 5) {
+    continue;
+  }
+  console.log(i);
+} */
+
+/* for (i = 1; i < 10; i++) {
+  if (i === 7) {
+    break;
+  }
+  console.log(i);
+}
+ */
