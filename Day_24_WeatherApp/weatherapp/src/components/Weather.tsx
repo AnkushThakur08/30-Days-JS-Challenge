183025c3e3c7450cfbfe2043a06f1501298aaab9
Ankush Thakur <66546775+AnkushThakur08@users.noreply.github.com>
2024-08-26 09:05:01 +0000

    Project setup in React

---
+++ Day_24_WeatherApp/weatherapp/src/components/Weather.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const Weather: React.FC = () => {
+    return (
+        <div>
+            <h1>Weather App </h1>
+        </div>
+    )
+}
+
+
+export default Weather
