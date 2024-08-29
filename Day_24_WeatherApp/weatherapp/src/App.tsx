6d4c113faeb3285aeff377e03487e74b6deeb9cb
Ankush Thakur <66546775+AnkushThakur08@users.noreply.github.com>
2024-08-29 06:23:52 +0000

    WIP

--- Day_24_WeatherApp/weatherapp/src/App.tsx
+++ Day_24_WeatherApp/weatherapp/src/App.tsx
@@ -2,16 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Weather from './components/Weather'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className="bg-[#74B9FF] h-screen">
+    <Weather />
       <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
-    </>
+    </div>
   )
 }
 
--- Day_24_WeatherApp/weatherapp/src/components/Weather.tsx
+++ Day_24_WeatherApp/weatherapp/src/components/Weather.tsx
@@ -1,13 +1,50 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
 
 const Weather: React.FC = () => {
+    const [input, setInput] = useState<string>("");
+    const [latitude, setLatitude] = useState<number | null>(null);
+    const [longitude, setLongitude] = useState<number | null>(null);
+
+    useEffect(() => {
+        if (!input) return; 
+
+        const getData = setTimeout(() => {
+            fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${input}&limit=5&appid=7e84badf24cbc95f292cf5eb72c895a8`)
+                .then((res) => res.json())
+                .then((data) => {
+                    console.log(data);
+                    if (data.length > 0) {
+                        setLatitude(data[0].lat);
+                        setLongitude(data[0].lon);
+                    }
+                })
+                .catch((error) => console.error("Geolocation fetch error:", error));
+        }, 2000);
+
+        return () => clearTimeout(getData);
+    }, [input]);
+
+    useEffect(() => {
+        if (latitude && longitude) {
+            fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=7e84badf24cbc95f292cf5eb72c895a8`)
+                .then((res) => res.json())
+                .then((data) => console.log("Weather data:", data))
+                .catch((error) => console.error("Weather fetch error:", error));
+        }
+    }, [latitude, longitude]);
+
     return (
         <div>
-            <h1>Weather App </h1>
+            <div>
+                <input
+                    type="text"
+                    name="input"
+                    onChange={(e) => setInput(e.target.value)}
+                    value={input}
+                />
+            </div>
         </div>
-    )
-}
+    );
+};
 
-
-export default Weather
+export default Weather;
